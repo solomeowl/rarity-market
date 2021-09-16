993c79c87c4216421469677dad107c88cb4cf09a
LeeChunHao2000 <meowinhere@gmail.com>
2021-09-17 02:39:47 +0800

    feat: add deploy scripts

--- test/1-CraftingMarket.js
+++ test/1-CraftingMarket.js
@@ -10,7 +10,7 @@ describe('#InitMarket', () => {
         deployer = accounts[0];
 
         const MarketFact = await ethers.getContractFactory("RarityCraftingMarket");
-        const Market = await upgrades.deployProxy(MarketFact, ["0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb", 1, 5])
+        const Market = await upgrades.deployProxy(MarketFact, ["0xf41270836dF4Db1D28F7fd0935270e3A603e78cC", 1, 5])
         await Market.deployed();
         console.log("RarityCraftingMarket deployed to:", Market.address);
     })
